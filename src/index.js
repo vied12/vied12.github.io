c4f8e884d73e7f51070cd8e2e7dceb2c0219cc93
Edouard Richard <edou4rd@gmail.com>
2017-11-21 23:37:43 +0100

    unregister the service worker

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-
+import { unregister } from './registerServiceWorker'
 import 'font-awesome/css/font-awesome.css'
 
 ReactDOM.render((
     <App />
 ), document.getElementById('root'));
-registerServiceWorker();
+unregister();
