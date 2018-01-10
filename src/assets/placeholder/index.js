d9280d54cc5e1a5e1925f7953975a68ae01fc80a
Edouard Richard <edou4rd@gmail.com>
2018-01-10 11:07:01 +0100

    placeholders

--- src/assets/placeholder/index.js
+++ src/assets/placeholder/index.js
@@ -1,34 +1,36 @@
-import superdeskPngSvg from './superdesk.png.svg';
-import loopedGifSvg from './looped.gif.svg';
-import smogalarmJpgSvg from './smogalarm.jpg.svg';
-import busGifSvg from './bus.gif.svg';
-import liveblogGifSvg from './liveblog.gif.svg';
+import brokenpromisesPngSvg from './brokenpromises.png.svg';
+import datawrapperGifSvg from './datawrapper.gif.svg';
+import spendingstoriesPngSvg from './spendingstories.png.svg';
+import wikileaksPngSvg from './wikileaks.png.svg';
 import tmfJpgSvg from './tmf.jpg.svg';
-import energyGifSvg from './energy.gif.svg';
 import detectiveGifSvg from './detective.gif.svg';
 import jeudinfluencesGifSvg from './jeudinfluences.gif.svg';
 import nost10YearsPngSvg from './nost10years.png.svg';
 import dragonsgiftsPngSvg from './dragonsgifts.png.svg';
-import datawrapperGifSvg from './datawrapper.gif.svg';
-import spendingstoriesPngSvg from './spendingstories.png.svg';
-import wikileaksPngSvg from './wikileaks.png.svg';
-import brokenpromisesPngSvg from './brokenpromises.png.svg';
+import liveblogGifSvg from './liveblog.gif.svg';
+import energyGifSvg from './energy.gif.svg';
+import busGifSvg from './bus.gif.svg';
 import looprGifSvg from './loopr.gif.svg';
+import superdeskPngSvg from './superdesk.png.svg';
+import loopedGifSvg from './looped.gif.svg';
+import smogalarmJpgSvg from './smogalarm.jpg.svg';
+import tbGifSvg from './tb.gif.svg';
 export {
-superdeskPngSvg,
-loopedGifSvg,
-smogalarmJpgSvg,
-busGifSvg,
-liveblogGifSvg,
+brokenpromisesPngSvg,
+datawrapperGifSvg,
+spendingstoriesPngSvg,
+wikileaksPngSvg,
 tmfJpgSvg,
-energyGifSvg,
 detectiveGifSvg,
 jeudinfluencesGifSvg,
 nost10YearsPngSvg,
 dragonsgiftsPngSvg,
-datawrapperGifSvg,
-spendingstoriesPngSvg,
-wikileaksPngSvg,
-brokenpromisesPngSvg,
+liveblogGifSvg,
+energyGifSvg,
+busGifSvg,
 looprGifSvg,
+superdeskPngSvg,
+loopedGifSvg,
+smogalarmJpgSvg,
+tbGifSvg
 }
